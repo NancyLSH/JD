cb89f8cfe5f26f589ddae5fd274004914cae0d69
NancyLSH <1052567432@qq.com>
2019-09-10 22:31:34 +0800

    09.10

--- static/index.js
+++ static/index.js
@@ -2,15 +2,16 @@ $("#listfore").hover(hoverShow, hoverHide)
 $(".right-sidebar").hover(hoverShow, hoverHide)
 window.onscroll = scroll
 
-function scroll(){
-    if(window.pageYOffset >= 700){
+function scroll() {
+    if (window.pageYOffset >= 700) {
         console.log("700")
         $(".right-menu").addClass("movedown")
     }
-    if(window.pageYOffset < 700){
+    if (window.pageYOffset < 700) {
         $(".right-menu").removeClass("movedown")
     }
 }
+
 function hoverShow() {
     $(".right-sidebar").css("display", "block")
     $("#listfore").css("background-color", "rgb(217,217,217)")
@@ -119,7 +120,7 @@ function onHover(i) {
 //左右按钮(要优化)
 function leftClick(i) {
     return function () {
-        console.log("i="+i)
+        console.log("i=" + i)
         clearTimer()
         var a
         if (window.i == 0) {
@@ -178,3 +179,112 @@ $("#cirbtn3").hover(onHover(2))
 $("#lbtn").click(leftClick(i))
 $("#rbtn").click(rightClick(i))
 
+//3
+var lpic = ["#bottomimg1", "#bottomimg2"]
+var rpic = ["#bottompic1", "#bottompic2"]
+$(lpic[1]).hide()
+$(rpic[1]).hide()
+
+function changepic(a) {
+    return function () {
+        console.log("done")
+        for (var i = 0; i < lpic.length; i++) {
+            $(lpic[i]).hide()
+            $(lpic[a]).show()
+            $(rpic[i]).hide()
+            $(rpic[a]).show()
+        }
+    }
+}
+
+$("#li1").hover(changepic(0))
+$("#li2").hover(changepic(1))
+
+//滚动条定时器(推翻一波)
+// mouse = null
+// onMouse = false
+// distance = 0
+// dis = 0
+// scroll = $("#scrollbtn").position().left;
+// $("#scrollbtn").mousedown(function () {
+//     var scroll = $("#scrollbtn").position().left;
+//     if (scroll < 0) {
+//         onMouse = false
+//         window.scroll = 0
+//     }
+//     if (scroll > 880) {
+//         onMouse = false
+//         window.scroll = 880
+//     } else {
+//         onMouse = true
+//     }
+//     if (onMouse) {
+//         mouse = window.event.clientX
+//         console.log("initial:" + mouse)
+//     }
+
+// })
+// $(document).mousemove(function () {
+//     var scroll = $("#scrollbtn").position().left;
+//     console.log("scroll:" + scroll)
+//     if (scroll < 0) {
+//         onMouse = false
+//         window.scroll = 0
+//     }
+//     if (scroll > 880) {
+//         onMouse = false
+//         window.scroll = 880
+//     }
+//     if (onMouse) {
+//         var newmouse = window.event.clientX
+//         distance = newmouse - mouse
+//         console.log("distance:" + distance)
+//         $("#scrollbtn").css("left", (dis + distance))
+//         // }
+
+//     }
+// })
+// $(document).mouseup(function () {
+//     onMouse = false
+//     var a = $("#scrollbtn").position().left
+//     console.log("up-scroll:"+a)
+//     if (a < 0) {
+//         window.scroll = 0
+//     }
+//     if (a > 880) {
+//         window.scroll = 800
+//     }
+//     console.log("up-scroll:"+window.scroll)
+//     dis += distance
+//     console.log("dis:" + dis)
+//     $("#scrollbtn").css("left", dis + "px")
+//     console.log(onMouse)
+//     console.log("newX:" + window.event.clientX)
+// })
+
+
+//滚动条 自动滚动
+function toLeft() {
+    $(".scroll-bar").addClass("toleft")
+}
+
+$(document).ready(function () {
+    toLeft()
+    $("#discover").hover(function () {
+        $(".scroll").css("opacity",1)
+        var x = parseFloat($("#scrollBar").css('transform').substring(7).split(',')[4])
+        console.log("x:"+x)
+        var scrollBtnX = (-1)*x*880/1746
+        console.log("scrollX:"+scrollBtnX)
+        $("#scrollbtn").css("transform","translateX(" + scrollBtnX + "px)")
+        $("#scrollbtn").css("transition","transform 0s")
+    },function(){
+        $(".scroll").css("opacity",0)
+    })
+})
+
+$("#scrollbtn").mousedown(function(){
+    var x = parseFloat($("#scrollBar").css('transform').substring(7).split(',')[4])
+    console.log("x:"+x)
+    $(".scroll-bar").removeClass("toleft")
+})
